9f028bf3a8aeb93a7886a49ad8e558cc2459a390
catuer <2131710111@student.polinema.ac.id>
2024-03-11 21:22:45 +0700

    Praktikum 2.7 – Relationships

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -4,12 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Monolog\Level;
 
 class UserModel extends Model
 {
     use HasFactory;
     protected $table = 'm_user';
     protected $primaryKey = 'user_id';
-    
-    protected $fillable = ['level_id', 'username','nama', 'password'];
+    protected $fillable =[
+        'level_id',
+        'username',
+        'nama',
+        'password'
+    ];
+    public function level(): BelongsTo{
+        return $this->BelongsTo(LevelModel::class, 'level_id', 'level_id');
+    }
 }
+
