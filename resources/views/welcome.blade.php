ba45af33b5ea39551d8652725ed9c8c44b7a29e4
catuer <2131710111@student.polinema.ac.id>
2024-03-21 10:57:43 +0700

    Praktikum 2 – Integrasi dengan DataTables

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,25 @@
 @extends('adminlte::page')
 
+{{--Customize layout sections--}}
+
 @section('title', 'Dashboard')
 
 @section('content_header')
-    <h1>Dashboard</h1>
+<h1>Dashboard</h1>
 @stop
 
 @section('content')
     <p>Welcome to this beautiful admin panel.</p>
 @stop
 
+{{--Push extra CSS--}}
+
 @section('css')
     {{-- Add here extra stylesheets --}}
     {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
 @stop
+
+{{-- Push extra scripts --}}
 
 @section('js')
     <script> console.log("Hi, I'm using the Laravel-AdminLTE package!"); </script>
